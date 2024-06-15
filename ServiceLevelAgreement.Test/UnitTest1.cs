fc922a3052beba75a495ee6c2e289366060fa645
Daniel Chagas <daniel.boasq@gmail.com>
2024-06-15 11:38:48 -0300

    feat(cases): new exception cases

--- ServiceLevelAgreement.Test/UnitTest1.cs
+++ ServiceLevelAgreement.Test/UnitTest1.cs
@@ -106,7 +106,11 @@ public class UnitTest1
     }
     
     [Theory]
-    [InlineData("2024-06-04 17:00:00", "2024-06-03 09:00:00")]
+    [InlineData("2024-06-02 09:00:00", "2024-06-01 09:00:00")]
+    [InlineData("2024-06-03 09:00:00", "2024-06-02 09:00:00")]
+    [InlineData("2024-06-04 09:00:00", "2024-06-03 09:00:00")]
+    [InlineData("2024-06-05 09:00:00", "2024-06-04 09:00:00")]
+    [InlineData("2024-06-06 09:00:00", "2024-06-05 09:00:00")]
     public void Should_Throw_ArgumentException_When_EndDate_IsMinorThan_StartDate(DateTime start, DateTime end)
     {
         // Arrange
